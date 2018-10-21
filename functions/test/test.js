876a4542bb2cf30d9090716352ac7711469f6042
jzhang <jzepitech@gmail.com>
2018-10-21 21:16:24 +0200

    Update test

--- functions/test/test.js
+++ functions/test/test.js
@@ -1,7 +1,14 @@
 const chai = require('chai');
+const index = require('./../index');
 const leboncoinHelper = require('./../leboncoin.helper');
 
-describe('Leboncoin Helper', () => {
+describe('Leboncoin Chatbot', () => {
+	describe('#Webhook Handler', () => {
+		it('should handle the webhook', () => {
+			index.handleConversation(); // Don't know what to test
+		});
+	});
+
 	describe('#getDialogflowParameters(agent)', () => {
 		it('should return a param object', () => {
 			const params = leboncoinHelper.getDialogflowParameters();
@@ -11,15 +18,16 @@ describe('Leboncoin Helper', () => {
 	});
 
 	describe('#createLeboncoinPromise(params)', () => {
-		it('should return a Promise', async () => {
+		it('should return a Promise', () => {
 			const params = {
 				propertyContract: 'locations',
 				propertyType: 'appartement',
 				department: 'paris',
 				zipCode: '75012',
 			};
-			const promise = await leboncoinHelper.createLeboncoinPromise(params);
-			chai.expect(typeof promise).to.equal('object');
+			leboncoinHelper.createLeboncoinPromise(params).then((promise) => {
+				chai.expect(typeof promise).to.equal('object');
+			});
 		});
 	});
 });
